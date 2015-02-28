51ac6d2ef63a1fb7255626fa39fd6661b090baf9
Dustin Bergman <dustin.bergman@gmail.com>
2015-02-28 17:08:08 -0500

    Updated controller styling

--- URBNEnvironmentPicker/URBNEnvironmentController.m
+++ URBNEnvironmentPicker/URBNEnvironmentController.m
@@ -30,76 +30,65 @@ static URBNEnvironmentController *_instance = nil;
 
 @implementation URBNEnvironmentController
 
-+ (URBNEnvironmentController *)sharedInstance
-{
-
++ (URBNEnvironmentController *)sharedInstance {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
 	    _instance = [[URBNEnvironmentController alloc] init];
     });
-
+    
     return _instance;
 }
 
 #pragma mark - Setup
-
-- (instancetype)init
-{
+- (instancetype)init {
     self = [super init];
+    
     if (self) {
-
         [self loadEnvironemntsFromPlist];
         [self loadCurrentEnvironemnt];
     }
+    
     return self;
 }
 
-- (void)loadCurrentEnvironemnt
-{
-
+- (void)loadCurrentEnvironemnt {
     URBNEnvironment *environment;
-
     NSString *saved = [self savedEnvironmentName];
 
     if (saved) {
-
         environment = [self environmentWithName:saved];
     }
 
     if (!environment) {
-
         environment = [self.availableEnvironments firstObject];
     }
 
     self.currentEnvironment = environment;
 }
 
-- (void)loadEnvironemntsFromPlist
-{
+- (void)loadEnvironemntsFromPlist {
+    NSURL *plistURL = [[NSBundle mainBundle] URLForResource:URBNEnvironemntsPlistName withExtension:URBNEnvironemntsPlistExtension];
 
-    NSURL *plistURL = [[NSBundle mainBundle] URLForResource:URBNEnvironemntsPlistName
-                                              withExtension:URBNEnvironemntsPlistExtension];
-
-    if (!plistURL)
+    if (!plistURL) {
         return;
+    }
 
     NSArray *environmentDictionaries = [NSArray arrayWithContentsOfURL:plistURL];
 
-    if (![environmentDictionaries isKindOfClass:[NSArray class]])
+    if (![environmentDictionaries isKindOfClass:[NSArray class]]) {
         return;
+    }
 
     NSMutableArray *environments = [NSMutableArray array];
-
     [environmentDictionaries enumerateObjectsUsingBlock:^(NSDictionary *obj, NSUInteger idx, BOOL *stop) {
-        
-        if(![obj isKindOfClass:[NSDictionary class]])
+        if (![obj isKindOfClass:[NSDictionary class]]) {
             return;
+        }
         
         NSError* __autoreleasing error;
         URBNEnvironment* environment = [URBNEnvironment modelWithDictionary:obj error:&error];
         
-        if(environment){
-            
+        if (environment) {
             [environments addObject:environment];
         }
     }];
@@ -108,36 +97,28 @@ static URBNEnvironmentController *_instance = nil;
 }
 
 #pragma mark - Utility Methods
-
-- (URBNEnvironment *)environmentWithName:(NSString *)name
-{
-
+- (URBNEnvironment *)environmentWithName:(NSString *)name {
     NSUInteger index = [[self availableEnvironments] indexOfObjectPassingTest:^BOOL(URBNEnvironment *obj, NSUInteger idx, BOOL *stop) {
-        
-        if([[obj name] isEqualToString:name]){
-            
+        if ([[obj name] isEqualToString:name]) {
             *stop = YES;
             return YES;
-            
         }
         return NO;
     }];
 
-    if (index == NSNotFound)
+    if (index == NSNotFound) {
         return nil;
+    }
 
     return [self.availableEnvironments objectAtIndex:index];
 }
 
 #pragma mark - Saved Environment
-
-- (NSString *)savedEnvironmentName
-{
+- (NSString *)savedEnvironmentName {
     return [[NSUserDefaults standardUserDefaults] objectForKey:URBNCurrentEnvironmentUserDefaultsKey];
 }
 
-- (void)saveEnvironmentName:(NSString *)name
-{
+- (void)saveEnvironmentName:(NSString *)name {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setObject:name
                  forKey:URBNCurrentEnvironmentUserDefaultsKey];
@@ -145,39 +126,25 @@ static URBNEnvironmentController *_instance = nil;
 }
 
 #pragma mark - Switch Environments
-
-- (void)changeToEnvironment:(URBNEnvironment *)newEnvironment
-{
-
-    if (!newEnvironment)
+- (void)changeToEnvironment:(URBNEnvironment *)newEnvironment {
+    if (!newEnvironment) {
         return;
+    }
 
     URBNEnvironment *oldEnvironment = [self currentEnvironment];
 
-    if ([oldEnvironment isEqual:newEnvironment])
+    if ([oldEnvironment isEqual:newEnvironment]) {
         return;
+    }
 
-    [[NSNotificationCenter defaultCenter] postNotificationName:URBNEnvironmentWillChangeNotification
-                                                        object:self
-                                                      userInfo:
-                                                          @{
-                                                             URBNEnvironmentChangeOldEnvironmentKey : oldEnvironment,
-                                                             URBNEnvironmentChangeNewEnvironmentKey : newEnvironment
-                                                          }];
+    [[NSNotificationCenter defaultCenter] postNotificationName:URBNEnvironmentWillChangeNotification object:self userInfo:@{URBNEnvironmentChangeOldEnvironmentKey : oldEnvironment, URBNEnvironmentChangeNewEnvironmentKey : newEnvironment}];
 
     self.currentEnvironment = newEnvironment;
     [self saveEnvironmentName:newEnvironment.name];
 
-    [[NSNotificationCenter defaultCenter] postNotificationName:URBNEnvironmentDidChangeNotification
-                                                        object:self
-                                                      userInfo:
-                                                          @{
-                                                             URBNEnvironmentChangeOldEnvironmentKey : oldEnvironment,
-                                                             URBNEnvironmentChangeNewEnvironmentKey : newEnvironment
-                                                          }];
+    [[NSNotificationCenter defaultCenter] postNotificationName:URBNEnvironmentDidChangeNotification object:self userInfo: @{URBNEnvironmentChangeOldEnvironmentKey : oldEnvironment, URBNEnvironmentChangeNewEnvironmentKey : newEnvironment}];
 
     if (self.terminateAppOnEnvironemntChange) {
-
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             abort();
         });
