0d0a580dfd609c15bfe65de142af34774980b234
Ray Migneco <ray.migneco@gmail.com>
2015-03-20 13:35:24 -0400

    Added umbrella file for imports

---
+++ URBNEnvironmentPicker.h
@@ -0,0 +1,14 @@
+/**
+    Umbrella file for importing all the associated files.
+    Import the header for any additions in this file
+*/
+
+#import <Foundation/Foundation.h>
+
+#import "URBNEnvironment.h"
+#import "URBNEnvironmentController.h"
+#import "URBNEnvironmentPickerTableViewController.h"
+
+@interface URBNEnvironmentPicker : NSObject
+
+@end
